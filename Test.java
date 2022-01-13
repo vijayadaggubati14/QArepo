13238cf5dd7aeb1a946559efa7c1c1da03c053b9
vijayadaggubati14 <97683162+vijayadaggubati14@users.noreply.github.com>
2022-01-13 07:55:24 -0700

    Add files via upload
    uploaded project files

---
+++ Test.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Encapsulation s=new Encapsulation();  
+		//setting value in the name member  
+		s.setName("vijaya");  
+		//getting value of the name member  
+		System.out.println(s.getName());
+
+	}
+
+}
